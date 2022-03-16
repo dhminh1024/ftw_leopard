839a9505715e48a4b338cac5255fa527dc5c719d
Minh Do <minhdh@coderschool.vn>
2022-03-16 14:15:46 +0700

    first release

--- week3/covid-report/script.js
+++ week3/covid-report/script.js
@@ -1,1 +1,21 @@
-console.log("Covid report");
+const getCovidStats = async () => {
+  document.querySelector("#loading").textContent = "Loading data...";
+  try {
+    const response = await fetch("https://api.covid19api.com/world/total");
+
+    if (response.ok) {
+      const data = await response.json();
+      document.querySelector("#cases").textContent = data.TotalConfirmed;
+      document.querySelector("#deaths").textContent = data.TotalDeaths;
+      document.querySelector("#recovered").textContent = data.TotalRecovered;
+      document.querySelector("#error").textContent = "";
+    } else {
+      document.querySelector("#error").textContent = "Server response error";
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+  document.querySelector("#loading").textContent = "";
+};
+
+getCovidStats();
