4a266411f64a2b2b68b30fd48881328731947f95
Minh Do <minhdh@coderschool.vn>
2022-04-05 11:54:35 +0700

    adding filter

--- coder-store/src/components/ProductFilter.js
+++ coder-store/src/components/ProductFilter.js
@@ -1,10 +1,47 @@
+import { Box, Button, Stack, Typography } from "@mui/material";
 import React from "react";
+import { FMultiCheckbox, FRadioGroup } from "./form";
 
-function ProductFilter() {
+const FILTER_GENDER_OPTIONS = ["Men", "Women", "Kids"];
+const FILTER_CATEGORY_OPTIONS = ["All", "Shoes", "Apparel", "Accessories"];
+const FILTER_PRICE_OPTIONS = [
+  { value: "below", label: "Below $25" },
+  { value: "between", label: "Between $25 - $75" },
+  { value: "above", label: "Above $75" },
+];
+
+function ProductFilter({ resetFilter }) {
   return (
-    <div>
-      <h1>ProductFilter</h1>
-    </div>
+    <Stack spacing={3}>
+      <Stack>
+        <Typography variant="h6" sx={{ fontWeight: 600 }}>
+          Gender
+        </Typography>
+        <FMultiCheckbox name="gender" options={FILTER_GENDER_OPTIONS} />
+      </Stack>
+
+      <Stack>
+        <Typography variant="h6" sx={{ fontWeight: 600 }}>
+          Category
+        </Typography>
+        <FRadioGroup name="category" options={FILTER_CATEGORY_OPTIONS} />
+      </Stack>
+
+      <Stack>
+        <Typography variant="h6" sx={{ fontWeight: 600 }}>
+          Price
+        </Typography>
+        <FRadioGroup
+          name="priceRange"
+          options={FILTER_PRICE_OPTIONS.map((item) => item.value)}
+          getOptionLabel={FILTER_PRICE_OPTIONS.map((item) => item.label)}
+        />
+      </Stack>
+
+      <Box>
+        <Button onClick={resetFilter}>Clear All</Button>
+      </Box>
+    </Stack>
   );
 }
 
--- coder-store/src/pages/HomePage.js
+++ coder-store/src/pages/HomePage.js
@@ -7,11 +7,32 @@ import Logo from "../components/Logo";
 import ProductFilter from "../components/ProductFilter";
 import ProductSearch from "../components/ProductSearch";
 import ProductSort from "../components/ProductSort";
+import { useForm } from "react-hook-form";
+import { FormProvider } from "../components/form";
+
+const defaultValues = {
+  gender: [],
+  category: "All",
+  priceRange: "",
+  sortBy: "featured",
+  searchQuery: "",
+};
 
 function HomePage() {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+
+  const methods = useForm({ defaultValues });
+  const { watch, reset } = methods;
+  const filters = watch();
+
+  useEffect(() => {
+    if (products.length) {
+      setFilteredProducts(applyFilter(products, filters));
+    }
+  }, [products, filters]);
 
   useEffect(() => {
     const getProducts = async () => {
@@ -31,7 +52,9 @@ function HomePage() {
   return (
     <Container sx={{ display: "flex", minHeight: "100vh", mt: 3 }}>
       <Stack>
-        <ProductFilter />
+        <FormProvider methods={methods}>
+          <ProductFilter resetFilter={reset} />
+        </FormProvider>
       </Stack>
       <Stack sx={{ flexGrow: 1 }}>
         <Stack
@@ -39,8 +62,10 @@ function HomePage() {
           direction={{ xs: "column", sm: "row" }}
           justifyContent="space-between"
         >
-          <ProductSearch />
-          <ProductSort />
+          <FormProvider methods={methods}>
+            <ProductSearch />
+            <ProductSort />
+          </FormProvider>
         </Stack>
         <Box sx={{ position: "relative", height: 1 }}>
           {loading ? (
@@ -50,7 +75,7 @@ function HomePage() {
               {error ? (
                 <Alert severity="error">{error}</Alert>
               ) : (
-                <ProductList products={products} />
+                <ProductList products={filteredProducts} />
               )}
             </>
           )}
@@ -58,6 +83,38 @@ function HomePage() {
       </Stack>
     </Container>
   );
+}
+
+function applyFilter(products, filters) {
+  let filteredProducts = products;
+
+  if (filters.gender.length) {
+    filteredProducts = filteredProducts.filter((product) =>
+      filters.gender.includes(product.gender)
+    );
+  }
+
+  if (filters.category !== "All") {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.category === filters.category
+    );
+  }
+
+  if (filters.priceRange) {
+    filteredProducts = filteredProducts.filter((product) => {
+      if (filters.priceRange === "below") {
+        return product.price < 25;
+      }
+      if (filters.priceRange === "between") {
+        return product.price >= 25 && product.price <= 75;
+      }
+      if (filters.priceRange === "above") {
+        return product.price > 75;
+      }
+    });
+  }
+
+  return filteredProducts;
 }
 
 export default HomePage;
