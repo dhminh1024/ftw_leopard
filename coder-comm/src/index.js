c27b5fd67f95dad4efb50dac0cbf6807dcfa2552
Minh Do <minhdh@coderschool.vn>
2022-04-15 16:06:19 +0700

    codercomm: setup authentication

--- coder-comm/src/index.js
+++ coder-comm/src/index.js
@@ -1,50 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-const a = {
-  accessToken:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU0NWUwMjNkOGRhNmZhZjJiN2E2MzciLCJpYXQiOjE2NDk5OTIzMzIsImV4cCI6MTY1MDA3ODczMn0.lSH326_bbMoCJlIth4uB7CK_tHwK82MsRKAeYvVjqZM",
-  user: {
-    aboutMe: "A curious learner.",
-    avatarUrl:
-      "http://res.cloudinary.com/ddawlo98i/image/upload/v1649697021/coder_comm/qd0h6slrvqvovdpmlnox.png",
-    city: "Ho Chi Minh City",
-    company: "CoderSchool",
-    country: "Vietnam",
-    coverUrl: "https://images.unsplash.com/photo-1508849789987-4e5333c12b78",
-    createdAt: "2022-04-11T16:57:38.641Z",
-    email: "minhdh@coderschool.vn",
-    facebookLink: "https://www.facebook.com/hai.m.do",
-    friendCount: 14,
-    instagramLink: "",
-    jobTitle: "Software Engineer",
-    linkedinLink: "https://www.linkedin.com/in/haiminhdo",
-    name: "Minh Hai Do",
-    postCount: 1,
-    twitterLink: "https://twitter.com/MinhDo87737628",
-    updatedAt: "2022-04-13T05:50:51.733Z",
-    __v: 0,
-    _id: "62545e023d8da6faf2b7a637",
-  },
-  message: "Login successful",
-  success: true,
-};
+root.render(<App tab="home" />);
---
+++ coder-comm/src/routes/index.js
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import BlankLayout from "../layouts/BlankLayout";
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import AuthRequire from "./AuthRequire";
+
+function Router() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <AuthRequire>
+            <MainLayout />
+          </AuthRequire>
+        }
+      >
+        <Route index element={<HomePage />} />
+      </Route>
+
+      <Route element={<BlankLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default Router;
